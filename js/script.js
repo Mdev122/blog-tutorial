d3826abb63d23d4bccde0ac45a67ba0fde19b426
Mdev122 <cimmerian422@gmail.com>
2026-06-16 10:07:12 +0200

    szablon skryptu

--- js/script.js
+++ js/script.js
@@ -50,3 +50,46 @@ console.log('Liczba znalezionych linków w menu bocznym:', links.length);
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+const generateTags = function() {
+  /* find all articles */
+  const articles = document.querySelectorAll('.posts .post');
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector('.post-tags .list');
+
+    /* check if wrapper exists to avoid script errors */
+    if (!tagsWrapper) continue;
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+
+      /* generate HTML of the link */
+      const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
+
+      /* add generated code to html variable */
+      html = html + linkHTML;
+
+    /* END LOOP: for each tag */
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+
+  /* END LOOP: for every article: */
+  }
+};
+
+// Uruchomienie funkcji generowania tagów
+generateTags();
